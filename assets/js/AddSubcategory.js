35d211af75acccfe0664003585bc76fb8638ecb3
Asmaa Reda <asmaaredaawad@gmail.com>
2017-01-28 13:02:39 +0200

    add assets

---
+++ assets/js/AddSubcategory.js
@@ -0,0 +1,28 @@
+$(function()
+{
+	$('#category').on("change",function(){
+		// d={};
+		selected_cID=$('#category option:selected').attr('value');
+		console.log(selected_cID);
+		$.ajax
+			({
+				url: "submit_subcategory.php?cat_id='"+selected_cID+"'",
+				type: 'POST',
+				dataType: 'json',
+				// data: d,
+				success: function (response) {
+					response = $.parseJSON(response);
+					console.log(response);
+					$('#subcategory').empty();
+					for (var i = 0; i < response.length; i++) {
+						$('#subcategory').append('<option value="'+response[i]['id']+'">'+response[i]['name']+'</option>');
+					}
+
+				},
+				error: function (error) {
+					console.log(error);
+				}
+			});
+	});
+
+});	
